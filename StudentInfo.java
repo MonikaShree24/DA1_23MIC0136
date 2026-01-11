bd53529d504b7a276fbfc2bcfab5bad172f9929d
Jaisurya20 <jaisurya8105@gmail.com>
2026-01-11 21:58:59 +0530

    First commit - Java program

---
+++ StudentInfo.java
@@ -0,0 +1,9 @@
+package DA1_23MIC0136;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Student Name: Monika Shree");
+        System.out.println("Reg no Number: 23MIC0136");
+        System.out.println("Message: Jenkins GitHub Integration Successful");
+    }
+}
