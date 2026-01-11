af40ed5f15a05372a3da33063dd511cabe561924
Jaisurya20 <jaisurya8105@gmail.com>
2026-01-11 22:08:28 +0530

    Removed package line

--- StudentInfo.java
+++ StudentInfo.java
@@ -1,9 +1,7 @@
-package DA1_23MIC0136;
-
 public class StudentInfo {
     public static void main(String[] args) {
         System.out.println("Student Name: Monika Shree");
-        System.out.println("Reg no Number: 23MIC0136");
+        System.out.println("Register Number: 23MIC0136");
         System.out.println("Message: Jenkins GitHub Integration Successful");
     }
 }
